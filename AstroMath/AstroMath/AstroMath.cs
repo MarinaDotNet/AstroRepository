1e3137cf64ed4af8c7fceb7f11d68ccd369dc7f1
Marina Sichova <35139158+msichova@users.noreply.github.com>
2023-05-01 12:45:09 +0800

    AstroMath
    library

---
+++ AstroMath/AstroMath/AstroMath.cs
@@ -0,0 +1,22 @@
+﻿namespace AstroMath
+{
+    public class AstroMath
+    {
+        public double StarVelocity(double observedWavelength, double restWavelength)
+        {
+            return (double)Math.Round((((observedWavelength - restWavelength) / restWavelength) * 299792458));
+        }
+        public double StarDistance(double arcsecondsAngle)
+        {
+            return (double)Math.Round((1 / arcsecondsAngle), 1);
+        }
+        public double TempretureInKelvin(double celsius)
+        {
+            return double.IsNegative(((double)celsius + 273.0)) ? 0.0 : ((double)celsius + 273.0);
+        }
+        public double EventHorizon(double BlackholeMass)
+        {
+            return Math.Round((((2 * (6.674 * Math.Pow(10, -11)) * BlackholeMass) / Math.Pow(299792458.0, 2)) / Math.Pow(10, 10)), 1);
+        }
+    }
+}
---
+++ AstroMath/AstroMath/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+/*
+ * 
+ * author: Marina Sichova
+ * student Id: P272494
+ * created: 27/03/2023
+ * last updated: 24/04/2023
+ * 
+ * Third party library AstroMath.dll with four mathematical formilas
+ */
+namespace AstroMath
+{
+    public class AstroData
+    {
+        public double StarVelocity(double observedWavelength, double restWavelength)
+        {
+            return (double)Math.Round((((observedWavelength - restWavelength) / restWavelength) * 299792458));
+        }
+        public double StarDistance(double arcsecondsAngle)
+        {
+            return (double)Math.Round((1 / arcsecondsAngle), 2);
+        }
+        public double TempretureInKelvin(double celsius)
+        {
+            return (((double)celsius + 273.0) <= 0.0) ? 0.0 : ((double)celsius + 273.0);
+        }
+        public double EventHorizon(double blackholeMass, int pow)
+        {
+            if (pow > 10)
+            {
+                pow -= 10;
+            }
+            double value = ((2 * 6.674 * (blackholeMass * Math.Pow(10, pow))) / Math.Pow(299792458, 2)) / 10;
+            value /= Math.Pow(10, 10);
+            return Math.Round(value, 1);
+        }
+    }
+}
